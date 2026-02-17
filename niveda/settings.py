cdcb339b0da7126e829d7aca8fb7f97342d0cd3f
Anasmajestic <anasmajestic@gmail.com>
2026-02-17 13:08:57 +0530

    rendertry1

--- niveda/settings.py
+++ niveda/settings.py
@@ -26,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q*u)*2ps7l-lsq4ms^w9ni@x$*$!d&nkvluioi5!+_b=4x&1%#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".onrender.com"]
 
-CORS_ALLOWED_ORIGINS = ["https://nivediaedu.info",]
+CORS_ALLOWED_ORIGINS = []
 
 CORS_ALLOW_ALL_ORIGINS=True
 
